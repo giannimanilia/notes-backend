7039f57b8da2f469b0c6ff2fd137b1d719a42486
Gianni Antonio Manilia <giannimanilia@gmail.com>
2020-09-25 21:36:57 +0200

    Initial commit

---
+++ src/Application.kt
@@ -0,0 +1,26 @@
+package com.gmaniliapp
+
+import io.ktor.application.*
+import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
+import io.ktor.gson.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.Routing
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing)
+    install(ContentNegotiation) {
+        gson {
+            setPrettyPrinting()
+        }
+    }
+}
+
---
+++ src/data/collection/Note.kt
@@ -0,0 +1,14 @@
+package com.gmaniliapp.data.collection
+
+import org.bson.codecs.pojo.annotations.BsonId
+import org.bson.types.ObjectId
+
+data class Note(
+    @BsonId
+    val id: String = ObjectId().toString(),
+    val title: String,
+    val content: String,
+    val date: Long,
+    val owners: List<String>,
+    val color: String
+)
