dc957e7abfde01729426114a1341b69b03cfb0f4
Gianni Antonio Manilia <giannimanilia@gmail.com>
2020-09-26 22:49:22 +0200

    changed registration endpoint

--- src/route/AuthRoute.kt
+++ src/route/AuthRoute.kt
@@ -1,5 +1,8 @@
 package com.gmaniliapp.route
 
+import com.gmaniliapp.data.checkIfUserExists
+import com.gmaniliapp.data.collection.User
+import com.gmaniliapp.data.insertUser
 import com.gmaniliapp.data.request.AccountRequest
 import com.gmaniliapp.service.login
 import io.ktor.application.*
@@ -10,6 +13,29 @@ import io.ktor.response.*
 import io.ktor.routing.*
 
 fun Route.authRoute() {
+    route("/rest/v1/auth/register") {
+        post {
+            val request = try {
+                call.receive<AccountRequest>()
+            } catch (exception: ContentTransformationException) {
+                call.respond(HttpStatusCode.BadRequest, "Bad request")
+                return@post
+            }
+
+            if (!checkIfUserExists(request.email)) {
+                if (insertUser(User(email = request.email, password = request.password))) {
+                    call.respond(HttpStatusCode.Created, "User successfully registered")
+                } else {
+                    call.respond(HttpStatusCode.InternalServerError, "Error registering user")
+                }
+            } else {
+                call.respond(
+                    HttpStatusCode.NotAcceptable,
+                    "There already exists an user associated to that mail"
+                )
+            }
+        }
+    }
     route("/rest/v1/auth/login") {
         post {
             val request = try {
--- src/route/UsersRoute.kt
+++ src/route/UsersRoute.kt
@@ -1,38 +1,6 @@
 package com.gmaniliapp.route
 
-import com.gmaniliapp.data.checkIfUserExists
-import com.gmaniliapp.data.collection.User
-import com.gmaniliapp.data.insertUser
-import com.gmaniliapp.data.request.AccountRequest
-import io.ktor.application.*
-import io.ktor.features.ContentTransformationException
-import io.ktor.http.*
-import io.ktor.request.*
-import io.ktor.response.*
 import io.ktor.routing.*
 
 fun Route.usersRoute() {
-    route("/rest/v1/users") {
-        post {
-            val request = try {
-                call.receive<AccountRequest>()
-            } catch (exception: ContentTransformationException) {
-                call.respond(HttpStatusCode.BadRequest, "Bad request")
-                return@post
-            }
-
-            if (!checkIfUserExists(request.email)) {
-                if (insertUser(User(email = request.email, password = request.password))) {
-                    call.respond(HttpStatusCode.Created, "User successfully registered")
-                } else {
-                    call.respond(HttpStatusCode.InternalServerError, "Error registering user")
-                }
-            } else {
-                call.respond(
-                    HttpStatusCode.NotAcceptable,
-                    "There already exists an user associated to that mail"
-                )
-            }
-        }
-    }
 }
